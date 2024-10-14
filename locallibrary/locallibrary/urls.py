0a8c0d695a51e28166b9b54cff03a2c7b601dbd4
milaurila <michael.laurila@gmail.com>
2024-10-14 18:23:20 +0200

    Add static serving

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import include, path
 # Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +32,7 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-        path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
